b39227189c3cc29a15a4e4c031d60ef9dc793bee
danieltheo90 <dtheo90@yahoo.com>
2018-03-15 20:25:12 +0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,16 @@ app.use(express.static(publicPath));
 io.on('connection',(socket)=>{
 	console.log('New User connected');
 	
+	socket.emit('newMessage',{
+		from: 'Admin',
+		text: 'Welcome to the chat app'
+	});
+
+	socket.broadcast.emit('newMessage',{
+		from: 'Admin',
+		text: 'New User joined',
+		createdAt: new Date().getTime()
+	})
 
 	socket.on('createMessage',(message)=>{
 		console.log('createMessage',message);
@@ -23,6 +33,7 @@ io.on('connection',(socket)=>{
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+		
 	});
 	socket.on('disconnect', () => {
     	console.log('User was disconnected');
